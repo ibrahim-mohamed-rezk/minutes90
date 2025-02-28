0d5f5f88c91e0e28258d3556000a9ac3586a370d
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-02-28 22:22:54 +0200

    ,,

--- components/profile/userProfile/Agent.tsx
+++ components/profile/userProfile/Agent.tsx
@@ -73,7 +73,7 @@ const Agent = () => {
           {/* user image */}
           <div className="w-full h-[306px] bg-white rounded-[30px] shadow-lg p-[10px]">
             <img
-              className="w-full h-full rounded-[30px] object-cover"
+              className="w-full h-full rounded-[30px]"
               src={data?.user?.image || "/images/icons/userAvatar.png"}
               alt="user image"
             />
